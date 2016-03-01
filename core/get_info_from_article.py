44b77e4ef6d42b9e9aafaaefb2fcc17310d1ffe6
solovyevdaniel <solovyev.daniel@gmail.com>
2016-03-01 20:58:57 +0200

    fix

--- core/get_info_from_article.py
+++ core/get_info_from_article.py
@@ -14,7 +14,7 @@ find_money_in_article = re.compile(pattern=money_pattern, flags=re.IGNORECASE)
 
 def get_info_place(named_entities):
     places = []
-    re_places = re.findall(GPE, str(named_entities))
+    re_places = re.findall(GPE, named_entities)
     if re_places:
         for item in re_places:
             if item[4:-1] not in places:
@@ -40,13 +40,12 @@ def get_info_money(article):
     result_bribe = re.findall(find_money_in_article, article)
     if result_bribe:
         for result in result_bribe:
-            if result[0] or result[3] or result[4]:
-                currency = result[0] or result[3] or result[4]
-            if result[1] or result[5]:
-                money_numbers = result[1] or result[5]
-                money_numbers_replaced = money_numbers.replace(',', '.')
-            if result[2]:
-                rank_digit = rank_str_to_digit(result[2])
+            currency = result[0] or result[3] or result[4]
+
+            money_numbers = result[1] or result[5]
+            money_numbers_replaced = money_numbers.replace(',', '.')
+
+            rank_digit = rank_str_to_digit(result[2])
 
             total_sum_of_bribe = money_numbers_replaced * rank_digit
             money.append((total_sum_of_bribe, currency))
@@ -54,6 +53,7 @@ def get_info_money(article):
 
 
 def article_processing(article):
+    named_entities = None
     try:
         for item in article:
             tokenized = nltk.word_tokenize(item)
@@ -62,6 +62,9 @@ def article_processing(article):
     except:
         return None
     else:
-        places = get_info_place(named_entities)
+        if named_entities:
+            places = get_info_place(str(named_entities))
+        else:
+            places = []
         money = get_info_money(article)
     return places, money
