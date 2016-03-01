9dfc30084111713327c08259fac95de23753a74b
solovyevdaniel <solovyev.daniel@gmail.com>
2016-03-01 21:38:57 +0200

    add model Bribe

--- core/models.py
+++ core/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
-#TODO: add migration
-#Todo Add table with bribe currency place, One-to-Many
+
 class Article(models.Model):
     newspaper = models.CharField(max_length=15)
     title = models.CharField(max_length=500)
     link = models.CharField(max_length=500)
     pub_date = models.DateTimeField()
 
-    bribe = models.CharField(max_length=30, blank=True, null=True)
-    currency = models.CharField(max_length=30, blank=True, null=True)
+
+class Bribe(models.Model):
     place = models.CharField(max_length=30, blank=True, null=True)
+    money = models.CharField(max_length=30, blank=True, null=True)
+    currency = models.CharField(max_length=30, blank=True, null=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
